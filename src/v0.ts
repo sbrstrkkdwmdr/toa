249a8d1f45530a86b7e7fcf912fbadf8b325ef2b
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2025-06-18 00:04:44 +1000

    add non-auth api routes

---
+++ src/v0.ts
@@ -0,0 +1,36 @@
+// for api
+
+export const placeholder = {};
+
+export function beatmapImages(mapSetId: string) {
+    return {
+        //smaller res of full/raw
+        thumbnail: `https://b.ppy.sh/thumb/${mapSetId}l.jpg`,
+        thumbnailLarge: `https://b.ppy.sh/thumb/${mapSetId}l.jpg`,
+
+        //full res of map bg
+        full: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/fullsize.jpg`,
+        raw: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/raw.jpg`,
+
+        //same width but shorter height
+        cover: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/cover.jpg`,
+        cover2x: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/cover@2x.jpg`,
+
+        //smaller ver of cover
+        card: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/card.jpg`,
+        card2x: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/card@2x.jpg`,
+
+        //square
+        list: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/list.jpg`,
+        list2x: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/list@2x.jpg`,
+
+        //shorter height ver of cover
+        slimcover: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/slimcover.jpg`,
+        slimcover2x: `https://assets.ppy.sh/beatmaps/${mapSetId}/covers/slimcover@2x.jpg`,
+
+    };
+}
+
+export function userAvatar(id: string) {
+    return `https://a.ppy.sh/` + id;
+}
