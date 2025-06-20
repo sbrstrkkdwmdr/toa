3b6669142ed0c56a4ff3391d3579ea30a702f4c8
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2025-06-20 22:33:00 +1000

    add public match routes

--- src/v2/matches.ts
+++ src/v2/matches.ts
@@ -1,3 +1,47 @@
 import * as apitypes from '../apitypes';
 import * as helper from '../helper';
 import { Dict } from '../types';
+
+export async function listing(i: {
+    sort?: 'id_desc' | 'id_asc',
+    limit?: number,
+    cursor_string?: string,
+}) {
+    if (!helper.allowed('public')) throw new Error('Missing scope: public');
+
+    const url = `/matches`;
+
+    const params = helper.setParams(i, {});
+
+    return await helper.requests.get_v2(url, params) as Promise<{
+        cursor: apitypes.Cursor,
+        cursor_string: apitypes.CursorString,
+        matches: apitypes.Match[],
+        params: {
+            limit: number,
+            sort: string;
+        };
+    }>;
+}
+
+export async function get(i: {
+    match_id: number,
+    before?: number,
+    after?: number,
+    limit?: number,
+}) {
+    if (!i.match_id) throw new Error('Missing match ID');
+    if (!helper.allowed('public')) throw new Error('Missing scope: public');
+
+    const url = `/matches/${i.match_id}`;
+
+    const params = helper.setParams(i, {}, ['before', 'after', 'limit',]);
+
+    return await helper.requests.get_v2(url, params) as Promise<{
+        match: apitypes.Match,
+        events: apitypes.MatchEvent[],
+        users: apitypes.User[],
+        first_event_id: number,
+        latest_event_id: number,
+    }>;
+}
